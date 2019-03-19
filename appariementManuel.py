a629a53765aafc00f909ff27311123aee42338d1
thvac9 <thomas.vachon.2@ulaval.ca>
2019-03-19 14:54:53 -0400

    Manque le positionnement manuel

--- appariementManuel.py
+++ appariementManuel.py
@@ -3,7 +3,7 @@ import imageio as io
 import rechauffement as meth
 import matplotlib.pyplot as plt
 
-# Création d'une matrice 4x3 pour les points de bases
+# Création d'une matrice Nx3 pour les points de bases
 def matriceNx3(ptsBase):
     A = []
     n = ptsBase.shape[0]
@@ -12,7 +12,7 @@ def matriceNx3(ptsBase):
         # point i
         np.put(pts, [0,1], ptsBase[i])
         np.put(pts, [2], 1)
-        A .append(pts)
+        A.append(pts)
     A = np.reshape(A, (n,3))
     return A
 
@@ -25,9 +25,9 @@ def matriceResolve(ptsBase, ptsProj):
         pts = np.zeros((2,9), dtype=int)
         # points i
         np.put(pts, [0,1,2], -1*(ptsBase[i]))
-        np.put(pts, [6,7,8], ptsBase[i]*ptsProj[i][0])
+        np.put(pts, [6,7,8], ptsBase[i]*ptsProj[i,0])
         np.put(pts, [12,13,14], -1*(ptsBase[i]))
-        np.put(pts, [15,16,17], ptsBase[i]*ptsProj[i][1])
+        np.put(pts, [15,16,17], ptsBase[i]*ptsProj[i,1])
         A.append(pts)
     A = np.reshape(A, (2*n,9))
     return A
@@ -44,14 +44,25 @@ def calculHomographie(ptsBase, ptsProj):
     h = np.reshape(h, (3,3))
     return h
 
+# # Création d'une image commune (Mosaïque)
+# def mosaique(img1, img2, img3):
+
 
 if __name__ == "__main__":
-    # np.set_printoptions(precision=3,suppress=True)
+    np.set_printoptions(precision=3,suppress=True)
 
     # Extraction des images
     img1 = io.imread('./1-PartieManuelle/Serie1/IMG_2415.jpg')
     img2 = io.imread('./1-PartieManuelle/Serie1/IMG_2416.jpg')
     img3 = io.imread('./1-PartieManuelle/Serie1/IMG_2417.jpg')
+
+    # img1 = io.imread('./1-PartieManuelle/Serie2/IMG_2427.jpg')
+    # img2 = io.imread('./1-PartieManuelle/Serie2/IMG_2426.jpg')
+    # img3 = io.imread('./1-PartieManuelle/Serie2/IMG_2425.jpg')
+
+    # img1 = io.imread('./1-PartieManuelle/Serie3/IMG_2409.jpg')
+    # img2 = io.imread('./1-PartieManuelle/Serie3/IMG_2410.jpg')
+    # img3 = io.imread('./1-PartieManuelle/Serie3/IMG_2411.jpg')
 
     # Extraction des points des images
     ptsBase1 = np.loadtxt("./1-PartieManuelle/Serie1/pts_serie1/pts2_12.txt", delimiter=",")
@@ -59,14 +70,48 @@ if __name__ == "__main__":
     ptsBase2 = np.loadtxt("./1-PartieManuelle/Serie1/pts_serie1/pts2_32.txt", delimiter=",")
     ptsProj2 = np.loadtxt("./1-PartieManuelle/Serie1/pts_serie1/pts3_32.txt", delimiter=",")
 
+    # ptsBase1 = np.loadtxt("./1-PartieManuelle/Serie2/pts_serie2/pts2_12.txt", delimiter=",")
+    # ptsProj1 = np.loadtxt("./1-PartieManuelle/Serie2/pts_serie2/pts1_12.txt", delimiter=",")
+    # ptsBase2 = np.loadtxt("./1-PartieManuelle/Serie2/pts_serie2/pts2_32.txt", delimiter=",")
+    # ptsProj2 = np.loadtxt("./1-PartieManuelle/Serie2/pts_serie2/pts3_32.txt", delimiter=",")
+
+    # ptsBase1 = np.loadtxt("./1-PartieManuelle/Serie3/pts_serie3/pts2_12.txt", delimiter=",")
+    # ptsProj1 = np.loadtxt("./1-PartieManuelle/Serie3/pts_serie3/pts1_12.txt", delimiter=",")
+    # ptsBase2 = np.loadtxt("./1-PartieManuelle/Serie3/pts_serie3/pts2_32.txt", delimiter=",")
+    # ptsProj2 = np.loadtxt("./1-PartieManuelle/Serie3/pts_serie3/pts3_32.txt", delimiter=",")
     
     H1 = calculHomographie(ptsProj1, ptsBase1)
-    H2 = calculHomographie(ptsProj2, ptsBase2)
+    H3 = calculHomographie(ptsProj2, ptsBase2)
 
-    imH = meth.appliqueTransformation(img1,H1)
-    plt.imshow(imH)
-    plt.show()
+    # imH1 = meth.appliqueTransformation(img1,H1)
+    # io.imwrite('S3imgH1.jpg', imH1)
+    # plt.imshow(imH1)
+    # plt.show()
+    imH1 = io.imread('./1-PartieManuelle/Serie1/imgH1.jpg')
+    print(imH1.shape)
 
-    imH = meth.appliqueTransformation(img3,H2)
-    plt.imshow(imH)
+    # imH3 = meth.appliqueTransformation(img3,H3)
+    # # io.imwrite('S3imgH3.jpg', imH3)
+    # plt.imshow(imH3)
+    # plt.show()
+    imH3 = io.imread('./1-PartieManuelle/Serie1/imgH3.jpg')
+    print(imH3.shape)
+
+    origin1 = np.matmul(H1,[0,0,1])/np.matmul(H1,[0,0,1])[2]
+    x1 = abs(origin1[0]).astype(int)
+    y1 = abs(origin1[1]).astype(int)
+    print(origin1)
+    origin2 = np.matmul(H3,[0,0,1])/np.matmul(H3,[0,0,1])[2]
+    x2 = abs(origin2[0]).astype(int)
+    y2 = abs(origin2[1]).astype(int)
+    print(origin2)
+
+    
+    imF = np.zeros((2500, 3200, 3), dtype=int)
+    # print(imF.shape)
+    imF[:imH1.shape[0],:imH1.shape[1],:] = imH1
+    # imF[y2+57:imH3.shape[0]+y2+57,x2+x1:imH3.shape[1]+x2+x1,:] = imH3
+    imF[y2:imH3.shape[0]+y2,x2+x1:imH3.shape[1]+x2+x1,:] = imH3
+    imF[y1:img2.shape[0]+y1,x1:img2.shape[1]+x1,:] = img2
+    plt.imshow(imF)
     plt.show()
